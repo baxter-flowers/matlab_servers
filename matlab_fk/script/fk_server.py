78e72d27c5082c22dfec029156dcb90015b720e6
Poppy-Demo <none@none.none>
2016-01-15 18:20:06 +0100

    Bugfixes fk server

--- matlab_fk/script/fk_server.py
+++ matlab_fk/script/fk_server.py
@@ -9,22 +9,23 @@ rospy.init_node('matlab_fk_server')
 side = 'left'
 
 kinematics = baxter_pykdl.baxter_kinematics(side)
-bridge = PythonInterface(matlab_flag='FKflagMatlab.txt',
-                         python_flag='FKflagPython.txt',
-                         matlab_file='FKjoints.json',
-                         python_file='FKxyz.json')
-joints = map(lambda joint: 'side_'+joint, ['s0', 's1', 'e0', 'e1', 'w0', 'w1', 'w2'])
+bridge = PythonInterface()
+bridge.matlab_flag='/tmp/matlab_bridge/FKmatlabflag.txt'
+bridge.python_flag='/tmp/matlab_bridge/FKpythonflag.txt'
+bridge.matlab_file='/tmp/matlab_bridge/FKjoints.json'
+bridge.python_file='/tmp/matlab_bridge/FKxyz.json'
+joints = map(lambda joint: side+'_'+joint, ['s0', 's1', 'e0', 'e1', 'w0', 'w1', 'w2'])
 
 rospy.loginfo("FK server ready to handle FK for {}_arm".format(side))
 
 while not rospy.is_shutdown():
     joint_values = bridge.read()
     rospy.loginfo(" {}_arm FK request: ".format(side) + str(joint_values))
-    fk_request = dict(zip(fk_request), joint_values)
+    fk_request = dict(zip(joints, joint_values))
     fk_result = kinematics.forward_position_kinematics(fk_request)
     position = list(fk_result[:3])
     orientation = list(fk_result[3:])
-    bridge.send()
+    bridge.send(position)
 
 
 
